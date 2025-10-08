e83b5c783b784a6432b90e0dc3e079b796a6cbc6
Name:________ <bennettNicolasM@gmail.com>
2025-10-08 10:39:35 -0400

    game

---
+++ demo.py
@@ -0,0 +1,13 @@
+#while True:
+#    user_in = input("enter a number: ")
+#    if not user_in.isnumeric():
+#        print("make sure it's an int")
+#    #print(f"you entered {user_in})
+    
+while True:
+    try:
+       user_in = int(input("enter a number: "))
+    except ValueError:
+       print("that wasn't right!")
+    print(user_in)
+        
